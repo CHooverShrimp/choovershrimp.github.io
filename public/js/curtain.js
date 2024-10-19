f9a8b6433e3e2d03826d6e2cca2196d21d79c1dd
CHooverShrimp <choovershrimp@gmail.com>
2024-10-19 15:46:35 -0700

    Big changes
    - Image adjustment
    - Quote adjustment
    - Overflow adjustment
    - CSS optimization

--- public/js/curtain.js
+++ public/js/curtain.js
@@ -1,3 +1,25 @@
+// Array of quotes
+const quotes = [
+    '"When I was a lad I clopped four dozen times." - Anon',
+    '"Sex with Purp." - Anon',
+    '"Portal to Equestria!" - Anon',
+    '"Why do all of these have dicks!?" - Anon',
+    '"We\'re anons to the core!" - Anon',
+    '"Hey, is that Lauren Faust?!" - Anon',
+];
+
+// Function to get a random quote
+function getRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+}
+
+// Display a random quote on page load
+document.addEventListener('DOMContentLoaded', function() {
+    const quoteElement = document.getElementById('Anon-quotes');
+    quoteElement.textContent = getRandomQuote();
+});
+
 document.getElementById('enter-button').addEventListener('click', function() {
     const fallingImage = document.getElementById('falling-image');
 
@@ -12,6 +34,9 @@ document.getElementById('enter-button').addEventListener('click', function() {
     setTimeout(() => {
         this.style.display = 'none';
     }, 500)
+    
+    // Disable hidden overflow, let user scroll
+    document.body.style.overflow = 'auto';
 
     // Hide the curtain container after the animation
     setTimeout(function() {
