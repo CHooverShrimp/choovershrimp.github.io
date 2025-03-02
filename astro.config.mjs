5eedf649610a87c34cfb856f53153dcf1f1b30d6
CHooverShrimp <choovershrimp@gmail.com>
2025-03-01 20:59:12 -0800

    le update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     site: 'https://CHooverShrimp.github.io',
+    vite: {    plugins: [tailwindcss()],  },
 })
